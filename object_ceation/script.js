529cfedcbd009fea88c3fae4442da8e5dced81a2
azizarrahman <azizarrahman01032003@gmail.com>
2024-11-02 17:16:27 +0600

    object homework done

---
+++ object_ceation/script.js
@@ -0,0 +1,20 @@
+
+var Book = new Object();
+
+Book.title = 'moyurakkhi';
+Book.author = 'Humayun ahmed';
+Book.year = '2010';
+
+console.log(Book['title']);
+console.log(Book['author']);
+console.log(Book['year']);
+
+var Book2 = new Object();
+
+Book2.title = 'Recharge Your Down bettery';
+Book2.author = 'jhankar mahbub';
+Book2.year = '2019';
+
+console.log(Book2);
+
+
