ca2b2fb0d8b9544e86a7321f8675acb97f50e855
azizarrahman <azizarrahman01032003@gmail.com>
2024-11-02 17:32:12 +0600

    decoration folder

---
+++ reversarray/reversarray.js
@@ -0,0 +1,26 @@
+
+
+myarray = ['apple', 'banana', 'apple', 'milk']
+
+// with loop
+reversearray = [];
+for (let i = myarray.length - 1; i >= 0; i--) {
+    reversearray.push(myarray[i]);
+}
+
+
+// Another Waay
+
+
+// console.log(myarray);
+// console.log(reversearray);
+
+// newreversarray = myarray.reverse();
+// console.log(newreversarray);
+
+// againnewarray = [...newreversarray].reverse();
+// console.log(againnewarray);
+
+
+//==============Everything as Possible=========================
+
