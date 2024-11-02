25dbc798c8782d1ef685ea6b32676253443d2cb9
azizarrahman <azizarrahman01032003@gmail.com>
2024-11-02 18:00:23 +0600

    2d array manupulation

---
+++ 2darray/twodymentionalarray.js
@@ -0,0 +1,6 @@
+one = ['one', 'two', 'three'];
+two = ['four', 'five', 'six'];
+three = ['seven', 'eight', 'nine'];
+
+myarray = [one, two, three];
+console.log(myarray); // Output: [["one", "two", "three"], ["four", "five", "six]]
